ed8c1997985c0215ea1f19ea8e2287ed31fb1a70
mohammadmusa <mohammadmusa.contractor@verse.in>
2018-11-06 18:13:09 +0530

    added top nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TabNavComponent } from './tab-nav/tab-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TabNavComponent
   ],
   imports: [
     BrowserModule,
