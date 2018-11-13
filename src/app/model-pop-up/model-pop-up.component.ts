72274472d6fbd7c535a9879c577370f1ed50e644
mohammadmusa <mohammadmusa.contractor@verse.in>
2018-11-13 18:08:15 +0530

    added popup

---
+++ src/app/model-pop-up/model-pop-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-model-pop-up',
+  templateUrl: './model-pop-up.component.html',
+  styleUrls: ['./model-pop-up.component.css']
+})
+export class ModelPopUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
